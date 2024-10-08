4f4385d0883822768142d006ebe78a8aabadd9e3
Muhammad Ariel Ariadi <ariel.ariadi123@gmail.com>
2024-10-08 18:52:35 +0700

    feat: connect to mongodb

--- server.js
+++ server.js
@@ -2,20 +2,23 @@ import express from 'express';
 import dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { logger } from './middleware/logger.js';
+import { logEvents, logger } from './middleware/logger.js';
 import errorHandler from './middleware/errorHandle.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import corsOptions from './config/corsOptions.js';
+import connectDB from './config/dbConn.js';
+import mongoose from 'mongoose';
 
 import rootRoute from './routes/root.js';
 
 dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3500;
 
 console.log(process.env.NODE_ENV);
 
-const app = express();
-const PORT = process.env.PORT || 3500;
+connectDB();
 
 app.use(logger);
 app.use(cors(corsOptions));
@@ -43,4 +46,15 @@ app.all('*', (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+mongoose.connection.once('open', () => {
+  console.log('Connected to MongoDB');
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
+
+mongoose.connection.on('error', (err) => {
+  console.log(err);
+  logEvents(
+    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+    'mongoErrLog.log',
+  );
+});
