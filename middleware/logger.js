f1d8c020b673053e693272ce1c6c5438b1de4927
Muhammad Ariel Ariadi <ariel.ariadi123@gmail.com>
2024-10-09 09:12:39 +0700

    fix: fix log error on vercel and try to fix vercel.json configuration

--- middleware/logger.js
+++ middleware/logger.js
@@ -9,20 +9,21 @@ const logEvents = async (message, logFileName) => {
   const dateTime = format(new Date(), 'yyyyMMdd\tHH:mm:ss');
   const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
 
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = path.dirname(__filename);
+  // const __filename = fileURLToPath(import.meta.url);
+  // const __dirname = path.dirname(__filename);
 
-  try {
-    if (!fs.existsSync(path.join(__dirname, '..', 'logs'))) {
-      await fsPromises.mkdir(path.join(__dirname, '..', 'logs'));
-    }
-    await fsPromises.appendFile(
-      path.join(__dirname, '..', 'logs', logFileName),
-      logItem,
-    );
-  } catch (err) {
-    console.log(err);
-  }
+  // try {
+  //   if (!fs.existsSync(path.join(__dirname, '..', 'logs'))) {
+  //     await fsPromises.mkdir(path.join(__dirname, '..', 'logs'));
+  //   }
+  //   await fsPromises.appendFile(
+  //     path.join(__dirname, '..', 'logs', logFileName),
+  //     logItem,
+  //   );
+  // } catch (err) {
+  //   console.log(err);
+  // }
+  console.log(logItem);
 };
 
 const logger = (req, res, next) => {
