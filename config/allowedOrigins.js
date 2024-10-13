399fa46a59a28d16b048f805367639a120cb971d
Muhammad Ariel Ariadi <ariel.ariadi123@gmail.com>
2024-10-13 07:07:17 +0700

    chore: allow origin port 4173

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,8 @@
 const allowedOrigins = [
   'http://localhost:8000',
   'http://127.0.0.1:8000',
+  'http://localhost:4173',
+  'http://127.0.0.1:4173',
   'http://localhost:5173',
   'http://127.0.0.1:5173',
 ];
